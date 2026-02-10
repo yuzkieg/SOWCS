62e97dec6d0d8d88ee341d82b02de4c95362543f
Gerid Kent Degara <degara03@gmail.com>
2026-02-10 12:38:16 +0800

    Project and Task Views

---
+++ IT15_SOWCS/Controllers/Projects.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT15_SOWCS.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: /Projects/Index
+        public IActionResult Projects()
+        {
+            // Set the title for the Navbar in the layout
+            ViewData["Title"] = "Projects";
+            return View();
+        }
+    }
+}
---
+++ IT15_SOWCS/Controllers/Tasks.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT15_SOWCS.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+    }
+}
